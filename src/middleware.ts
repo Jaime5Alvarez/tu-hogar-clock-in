79c2112483a58b78a8845ea01abaa653e46be409
Jaime <jaime5alvarez5h@gmail.com>
2025-05-03 17:50:30 +0200

    refactor middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getSessionCookie } from "better-auth/cookies";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
 
 export async function middleware(request: NextRequest) {
-  const sessionCookie = getSessionCookie(request);
-  if (!sessionCookie) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
-
   return NextResponse.next();
 }
 
